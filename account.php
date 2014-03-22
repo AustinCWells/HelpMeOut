f5250a2a664332004c8ad55da1ace8c96d01c06a
Jordan Silver <Jordan@Jordans-MacBook-Pro.local>
2014-03-22 14:45:14 -0500

    Hour 3 Changes

---
+++ account.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>HelpMeOut</title>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script src="js/jquery.cookie.js"></script>
+	<!--<script language="javascript" type="text/javascript" src="js/index.js"> </script> 
+	<script language="javascript"  type="text/javascript" src="js/modal.js"> </script>  
+	<script language="javascript"  type="text/javascript" src="js/nav.js"> </script> -->
+	<link href="css/main.css" rel="stylesheet">
+
+</head>
+<body>
+	<?php include "components/nav.html"; ?> 
+	<p>WE WILL AWARD YOU SHINY NEW BADGES. SOON.</p>
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>HelpMeOut</title>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<!--<script src="js/jquery.cookie.js"></script>
+	<script language="javascript"  type="text/javascript" src="js/modal.js"> </script>  
+	<script language="javascript"  type="text/javascript" src="js/nav.js"> </script> -->
+	<link href="css/main.css" rel="stylesheet">
+	<script language="javascript" type="text/javascript" src="js/index.js"> </script> 
+
+	<!--TABS: -->
+	<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.1/themes/base/jquery-ui.css" />
+	<script src="http://code.jquery.com/ui/1.10.1/jquery-ui.js"></script>
+
+
+</head>
+<body>
+	<?php include "components/nav.html"; ?> 
+	<p>WE WILL HELP YOU OUT. SOON.</p>
+
+
+<div id="tabs">
+	<ul>
+		<!--This section introduces the tabs and titles them-->
+		<li><a href="#tabs-1">Food Pickup & Delivery</a></li>
+		<li><a href="#tabs-2">Laundry</a></li>
+		<li><a href="#tabs-3">Groceries</a></li>
+		<li><a href="#tabs-4">Transportation</a></li>
+		<li><a href="#tabs-5">Rides</a></li>
+	</ul>
+
+	<!--This section creates the content within each tab-->
+	<div id="tabs-1">
+		Insert content for first tab here.
+	</div>
+	<div id="tabs-2">
+		Insert content for second tab here.
+	</div>
+	<div id="tabs-3">
+		Insert content for third tab here.
+	</div>
+	<div id="tabs-4">
+		Insert content for fourth tab here.
+	</div>
+	<div id="tabs-5">
+		Insert content for fifth tab here.
+	</div>
+
+</div>
+
+</body>
+</html>
