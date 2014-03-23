177097ef54ae3d469928d83792f932a9e454fd04
Chris Dunn <christopherdunn711@gmail.com>
2014-03-23 00:52:56 -0500

    add more stuff to formSubmit

--- js/formSubmit.js
+++ js/formSubmit.js
@@ -7,4 +7,29 @@ $(document).ready(function(){
 
 	});
 
+	$(".loginForm").submit(function(){
+
+		var email = $(this).children("#loginName").val();
+		var password = $(this).children("#loginPassword").val();
+		console.log(email + "\n"  + password);
+
+	});
+
+	$(".accountForm").submit(function(){
+
+		var accountInfo = {};
+
+		accountInfo.firstName = $(this).children("#accountFName").val();
+		accountInfo.lastName = $(this).children("#accountLName").val();
+		accountInfo.number = $(this).children("#accountPhoneNumber").val();
+		accountInfo.email = $(this).children("#accountEmail").val();
+		var password = $(this).children().val();
+		var confirmPassword = $(this).children().val();
+		accountInfo.card = $(this).children.val();
+		accountInfo.cardNumber = $(this).children.val();
+
+		console.log(accountInfo);
+
+	});
+
 });
