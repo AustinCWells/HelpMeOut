3d5ab43fb3ae69fbc8c984deff2a22f2f9db4c6a
Jordan Silver <Jordan@Jordans-MacBook-Pro.local>
2014-03-23 04:10:40 -0500

    made current page's nav element non-hoverable

--- js/nav.js
+++ js/nav.js
@@ -12,12 +12,15 @@ window.addEventListener('load', function(event) {
 	var page = document.getElementsByClassName("currentPage")[0].getAttribute("id");
 	if(page === "indexPage") {
 		document.getElementById("indexHeader").style.background="rgb(34,46,64)";
+		$("#indexHeader").removeClass("hoverable");
 	}
 	if(page === "dashboardPage") {
 		document.getElementById("dashboardHeader").style.background="rgb(34,46,64)";
+		$("#dashboardHeader").removeClass("hoverable");
 	}
 	if(page == "accountPage") {
 		document.getElementById("accountHeader").style.background="rgb(34,46,64)";
+		$("#accountHeader").removeClass("hoverable");
 	}
 	
 }, false);
