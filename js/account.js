7861fd48ae3a74e91e7b682dd83ee0741be0263b
Jordan Silver <Jordan@Jordans-MacBook-Pro.local>
2014-03-23 01:55:18 -0500

    i'm really sorry spencer

--- js/account.js
+++ js/account.js
@@ -6,8 +6,8 @@ window.addEventListener('load', function(event) {
 	$(ratingBar).width(percent.innerHTML);
 
 
-	$("#profilePic").attr("src", "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-ash3/t1.0-1/p200x200/1606987_10152195066022415_1956973447_n.jpg");
-	var contactInfoHTML =  "Name: " + "Jordan Silver" + "<br>Phone #: " + "you can't have it" + "<br>Email: " + "jcsilver@smu.edu" + "<br>Location: " + "Table24";
+	$("#profilePic").attr("src", "img/spencer.png");
+	var contactInfoHTML =  "Name: " + "Spencer Kaiser" + "<br>Phone #: " + "you can't have it" + "<br>Email: " + "spencebro@smu.edu" + "<br>Location: " + "Table24";
 	$("#contactInfo").append(contactInfoHTML); 
 
 	var badges = [1,2,3];
