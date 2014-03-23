0cb327b856cd01cfa90df586b910642f71f22e4b
Jordan Silver <Jordan@Jordans-MacBook-Pro.local>
2014-03-23 00:30:41 -0500

    added sorry message when no jobs are available, populate recent jobs

--- index.php
+++ index.php
@@ -21,55 +21,30 @@
 		<li><a href="#tabs-1">Food Pickup & Delivery</a></li>
 		<li><a href="#tabs-2">Laundry</a></li>
 		<li><a href="#tabs-3">Groceries</a></li>
-		<li><a href="#tabs-4">Transportation</a></li>
+		<li><a href="#tabs-4">Tutoring</a></li>
 		<li><a href="#tabs-5">Rides</a></li>
 	</ul>
 
 	<!--This section creates the content within each tab-->
 	<div id="tabs-1">
-		<div class="postings" id="food">
-			<!--example job posting
-			<a href="http://facebook.com">
-				<div class="jobPost" id="foodPosting0">
-					<p class="jobTitle">I AM NOT HAUNGREH</p>
-					<p class="jobPrice">ONE MILLION DOLLARS</p>
-					<img class="jobImage" src="http://www.vectors4all.net/preview/closed-mailing-envelope-clip-art.jpg">
-				</div>
-			</a>
-			-->
-		</div>
+		<div class="postings" id="food"></div>
 	</div>
 	<div id="tabs-2">
-		<div class="postings" id="laundry">
-		</div>
+		<div class="postings" id="laundry"></div>
 	</div>
 	<div id="tabs-3">
-		<div class="postings" id="groceries">
-		</div>
+		<div class="postings" id="groceries"></div>
 	</div>
 	<div id="tabs-4">
-		<div class="postings" id="transportation">
-		</div>
+		<div class="postings" id="tutoring"></div>
 	</div>
 	<div id="tabs-5">
-		<div class="postings" id="rides">
-		</div>
+		<div class="postings" id="rides"></div>
 	</div>
 
 </div>
 
-<div id="recentJobs">
-RECENT JOBS:<br>
-
-JOBS<br>
-JOBS<br>
-JOBS<br>
-JOBS<br>
-<img src="http://s3.amazonaws.com/crunchbase_prod_assets/assets/images/resized/0001/0974/10974v7-max-250x250.jpg"><br>
-JOBS<br>
-JOBS<br>
-JOBS<br>
-</div>
+<div id="recentJobs"></div>
 
 </body>
 </html>
--- js/jobTabs.js
+++ js/jobTabs.js
@@ -7,31 +7,50 @@ window.addEventListener('load', function(event) {
 	for(var i=1;i<11;i++) {
 		var html = '<a href="' + "index.php" + '"><div class="jobPost" id="foodPosting' + i + '"><p class="jobTitle">' + "I AM HAUNGREH" + '</p><p class="jobPrice">' + "$" + "5" + '</p><img class="jobImage" src="' + 'img/food.png' + '"></div></a>';
 		$('#food').append(html);
-		console.log(i);
+	}
+	if($('#food').html() === '') {
+		$('#food').append('Sorry, there are currently no jobs available in this category.');
 	}
 
 	for(var i=1;i<11;i++) {
 		var html = '<a href="' + "index.php" + '"><div class="jobPost" id="laundryPosting' + i + '"><p class="jobTitle">' + "WASH MY SOCKS" + '</p><p class="jobPrice">' + "$" + "5" + '</p><img class="jobImage" src="' + 'img/cleaning.png' + '"></div></a>';
 		$('#laundry').append(html);
-		console.log(i);
+	}
+	if($('#laundry').html() === '') {
+		$('#laundry').append('Sorry, there are currently no jobs available in this category.');
 	}
 
 	for(var i=1;i<11;i++) {
 		var html = '<a href="' + "index.php" + '"><div class="jobPost" id="groceriesPosting' + i + '"><p class="jobTitle">' + "KROGER RUN 4 ME" + '</p><p class="jobPrice">' + "$" + "5" + '</p><img class="jobImage" src="' + 'img/groceries.png' + '"></div></a>';
 		$('#groceries').append(html);
-		console.log(i);
+	}
+	if($('#groceries').html() === '') {
+		$('#groceries').append('Sorry, there are currently no jobs available in this category.');
 	}
 
-	for(var i=1;i<11;i++) {
-		var html = '<a href="' + "index.php" + '"><div class="jobPost" id="transportationPosting' + i + '"><p class="jobTitle">' + "ISNT THIS THE SAME AS RIDES" + '</p><p class="jobPrice">' + "$" + "5" + '</p><img class="jobImage" src="' + 'img/rides.png' + '"></div></a>';
-		$('#transportation').append(html);
-		console.log(i);
+	for(var i=1;i<1;i++) {
+		var html = '<a href="' + "index.php" + '"><div class="jobPost" id="tutoringPosting' + i + '"><p class="jobTitle">' + "ISNT THIS THE SAME AS RIDES" + '</p><p class="jobPrice">' + "$" + "5" + '</p><img class="jobImage" src="' + 'img/rides.png' + '"></div></a>';
+		$('#tutoring').append(html);
+	}
+	if($('#tutoring').html() === '') {
+		$('#tutoring').append('Sorry, there are currently no jobs available in this category.');
 	}
 
 	for(var i=1;i<11;i++) {
 		var html = '<a href="' + "index.php" + '"><div class="jobPost" id="ridesPosting' + i + '"><p class="jobTitle">' + "ISNT THIS THE SAME AS TRANSP." + '</p><p class="jobPrice">' + "$" + "5" + '</p><img class="jobImage" src="' + 'img/rides.png' + '"></div></a>';
 		$('#rides').append(html);
-		console.log(i);
+	}
+	if($('#rides').html() === '') {
+		$('#rides').append('Sorry, there are currently no jobs available in this category.');
+	}
+
+	console.log('|' + $('#recentJobs').html() + '|');
+	for(var i=1;i<1;i++) {
+		var html = '<a href="' + "index.php" + '"><div class="jobPost" id="ridesPosting' + i + '"><p class="jobTitle">' + "ISNT THIS THE SAME AS TRANSP." + '</p><p class="jobPrice">' + "$" + "5" + '</p><img class="jobImage" src="' + 'img/rides.png' + '"></div></a>';
+		$('#recentJobs').append(html);
+	}
+	if($('#recentJobs').html() === '') {
+		$('#recentJobs').append('Sorry, there are no recent jobs.<br><img src="http://s3.amazonaws.com/crunchbase_prod_assets/assets/images/resized/0001/0974/10974v7-max-250x250.jpg">');
 	}
 
 
