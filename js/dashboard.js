231fe902a8bbc256b362cf869fdbdb33c3648774
Jordan Silver <Jordan@Jordans-MacBook-Pro.local>
2014-03-22 20:52:29 -0500

    Added javscript for loading jobs on dashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,4 +1,34 @@
-  $(function() {
-    $( "#accordionLeft" ).accordion();
-    $( "#accordionRight" ).accordion();
-  });
+
+
+window.addEventListener('load', function(event) {
+	var accordionLeft = $('#accordionLeft');
+	var accordionRight = $('#accordionRight');
+
+	var jobsList = [1,2,3];
+	for(var i=0;i<jobsList.length;i++) {
+		if(i===0) {
+			accordionLeft.empty();
+		}
+		var html = '<h3>Job #' + jobsList[i] + '</h3><div><p>Here is the description for Job #' + jobsList[i] + '!</p></div>';
+		accordionLeft.append(html);
+
+	}
+	
+	for(var i=0;i<jobsList.length;i++) {
+		if(i===0) {
+			accordionRight.empty();
+		}
+		var html = '<h3>Job #' + jobsList[i] + '</h3><div><p>Here is the description for Job #' + jobsList[i] + '!</p></div>';
+		accordionRight.append(html);
+
+	}
+
+
+
+	$(function() {
+		$( "#accordionLeft" ).accordion();
+		$( "#accordionRight" ).accordion();
+	});
+
+
+}, false);
