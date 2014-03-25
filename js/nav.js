463673e3e7c1a9757c42eea6fd3247c7151b9c94
Chris Dunn <christopherdunn711@gmail.com>
2014-03-25 11:46:16 -0500

    updates to nav and modal stuff

--- js/nav.js
+++ js/nav.js
@@ -1,29 +1,38 @@
-window.addEventListener('load', function(event) {
+$(window).ready(function(event) {
 
-	var modalLinks = document.getElementsByClassName("modalLink");
-	
-	for(var i=0;i<modalLinks.length;i++) {
-		$(modalLinks[i]).click(function(event) {
-			 event.preventDefault();
-			}
-		);
+	$.cookie.json = true;
+
+	var userInfo = $.cookie("userInfo");
+
+	userInfo = {};
+
+	if(userInfo === undefined){
+		alert("Your are not loged in.");
 	}
 
-	var page = document.getElementsByClassName("currentPage")[0].getAttribute("id");
-	if(page === "indexPage") {
-		document.getElementById("indexHeader").style.background="rgb(34,46,64)";
-		document.getElementById("indexHeader").getElementsByTagName('a')[0].style.color="rgb(255,255,255)";
-		$("#indexHeader").removeClass("hoverable");
-	}
-	if(page === "dashboardPage") {
-		document.getElementById("dashboardHeader").style.background="rgb(34,46,64)";
-		document.getElementById("dashboardHeader").getElementsByTagName('a')[0].style.color="rgb(255,255,255)";
-		$("#dashboardHeader").removeClass("hoverable");
-	}
-	if(page == "accountPage") {
-		document.getElementById("accountHeader").style.background="rgb(34,46,64)";
-		document.getElementById("accountHeader").getElementsByTagName('a')[0].style.color="rgb(255,255,255)";
-		$("#accountHeader").removeClass("hoverable");
+
+	else{
+
+		console.log("here");
+
+
+		var page = document.getElementsByClassName("currentPage")[0].getAttribute("id");
+		if(page === "indexPage") {
+			document.getElementById("indexHeader").style.background="rgb(34,46,64)";
+			document.getElementById("indexHeader").getElementsByTagName('a')[0].style.color="rgb(255,255,255)";
+			$("#indexHeader").removeClass("hoverable");
+		}
+		if(page === "dashboardPage") {
+			document.getElementById("dashboardHeader").style.background="rgb(34,46,64)";
+			document.getElementById("dashboardHeader").getElementsByTagName('a')[0].style.color="rgb(255,255,255)";
+			$("#dashboardHeader").removeClass("hoverable");
+		}
+		if(page == "accountPage") {
+			document.getElementById("accountHeader").style.background="rgb(34,46,64)";
+			document.getElementById("accountHeader").getElementsByTagName('a')[0].style.color="rgb(255,255,255)";
+			$("#accountHeader").removeClass("hoverable");
+		}
+
 	}
 	
-}, false);
+});
