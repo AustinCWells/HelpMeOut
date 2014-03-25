85bc42a61b5e7f98b361ec1a0b24cfe5dee24188
Chris Dunn <christopherdunn711@gmail.com>
2014-03-25 11:56:34 -0500

    added more nav stuff

--- js/nav.js
+++ js/nav.js
@@ -15,24 +15,6 @@ $(window).ready(function(event) {
 
 		console.log("here");
 
-
-		var page = document.getElementsByClassName("currentPage")[0].getAttribute("id");
-		if(page === "indexPage") {
-			document.getElementById("indexHeader").style.background="rgb(34,46,64)";
-			document.getElementById("indexHeader").getElementsByTagName('a')[0].style.color="rgb(255,255,255)";
-			$("#indexHeader").removeClass("hoverable");
-		}
-		if(page === "dashboardPage") {
-			document.getElementById("dashboardHeader").style.background="rgb(34,46,64)";
-			document.getElementById("dashboardHeader").getElementsByTagName('a')[0].style.color="rgb(255,255,255)";
-			$("#dashboardHeader").removeClass("hoverable");
-		}
-		if(page == "accountPage") {
-			document.getElementById("accountHeader").style.background="rgb(34,46,64)";
-			document.getElementById("accountHeader").getElementsByTagName('a')[0].style.color="rgb(255,255,255)";
-			$("#accountHeader").removeClass("hoverable");
-		}
-
 	}
 	
 });
