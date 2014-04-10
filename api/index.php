77679212987ae075bd719d77b7888ad85a7af8ac
SpencerKaiser <skaiser@me.com>
2014-04-10 15:37:31 -0500

    Splitting functions from index.php into two seperate files for simultaneous group editing without merge errors

--- api/index.php
+++ api/index.php
@@ -2,6 +2,7 @@
 	
 	require '../vendor/autoload.php';
 	require 'config.php';
+	require 'index2.php';
 
 	$app = new \Slim\Slim();
 
@@ -10,8 +11,9 @@
 	$app->get('/jobs',  'pullJobs');
 	$app->get('/jobsImDoing/:id', 'getJobsImDoing');
 	$app->get('/jobsINeedDone/:id', 'getsJobsINeedCompleted');
-	$app->post('/postatask', 'postTask');
+	$app->post('../index2/postatask', 'postTask');
 	$app->get('/recentTasks/:num_tasks', 'recentTasks');
+	$app->post('/addTokens', 'addTokens');
 	//$app->post('/paymentinfo', 'getPaymentInfo');
 
 	$app->run();
@@ -439,7 +441,53 @@
 	#	OUTPUTS:		JSON(task_id, beggar_id, chooser_id, short_description, notes, price, category_id, negotiable, time_frame_date, time_frame_date, location, date_posted)
 	#	STATUS:			NEEDS TESTING
     ##########
-	function recentTasks($num_tasks)
+#	function recentTasks($num_tasks)
+#	{
+#	$request = \Slim\Slim::getInstance()->request();
+#	$sql = "SELECT * FROM TASK GROUP BY task_id ORDER BY MAX(date_posted) LIMIT :num_tasks";
+#	try
+#		{
+#			$num_tasks = intval($num_tasks);
+#			$db = getConnection();
+#			$stmt= $db->prepare($sql);
+#			$stmt->bindParam('num_tasks', $num_tasks, PDO::PARAM_INT);
+#			$stmt->execute();
+#			$recentTasks = null;
+#			while($row = $stmt->fetch(PDO::FETCH_ASSOC)) //I'm not 100% sure about this line but I'm using login as a guide for this
+#			{
+#				$taskID = $row['task_id'];
+#				$recentTasks[$taskID] = array('beggar_id' => (int)$row['beggar_id'], 
+#											  'chooser_id' => (int)$row['chooser_id'], 
+#											  'short_description' => $row['short_description'], 
+#											  'notes' => $row['notes'], 
+#											  'price' => (int)$row['price'], 
+#											  'category_id' => (int)$row['category_id'], 
+#											  'negotiable' => (int)$row['negotiable'], 
+#											  'time_frame_date' => $row['time_frame_date'], 
+#											  'time_frame_time' => $row['time_frame_time'], 
+#											  'location' => $row['location'],
+#											  'date_posted' => $row['date_posted']);
+#			}
+#			$db = null;
+#		   	echo json_encode($recentTasks);
+#	      	
+#      	}
+#	catch(PDOException $e) 
+#		{
+#			echo '{"error":{"text":'. $e->getMessage() .'}}'; 
+#		}
+#	}
+
+
+	##########
+	# 	AUTHOR:			Spencer
+	#	LAST UPDATED:	4/10/14
+	#	SUMMARY:		Pulls a specified number of active jobs based on recency (most recent tasks are rated highest)
+	#	INPUTS:			INT num_tasks
+	#	OUTPUTS:		JSON(task_id, beggar_id, chooser_id, short_description, notes, price, category_id, negotiable, time_frame_date, time_frame_date, location, date_posted)
+	#	STATUS:			NEEDS TESTING
+    ##########
+	function addTokens()
 	{
 	$request = \Slim\Slim::getInstance()->request();
 	$sql = "SELECT * FROM TASK GROUP BY task_id ORDER BY MAX(date_posted) LIMIT :num_tasks";
@@ -475,8 +523,6 @@
 			echo '{"error":{"text":'. $e->getMessage() .'}}'; 
 		}
 	}
-
-
 
 
 
