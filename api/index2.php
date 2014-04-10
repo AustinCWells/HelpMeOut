a6859246683745b3434b8fe1a6ea2d612cc9eaae
SpencerKaiser <skaiser@me.com>
2014-04-10 16:04:06 -0500

    Modifying addTokens; saving progress

--- api/index2.php
+++ api/index2.php
@@ -49,40 +49,25 @@
 	##########
 	# 	AUTHOR:			Spencer
 	#	LAST UPDATED:	4/10/14
-	#	SUMMARY:		Pulls a specified number of active jobs based on recency (most recent tasks are rated highest)
-	#	INPUTS:			INT num_tasks
-	#	OUTPUTS:		JSON(task_id, beggar_id, chooser_id, short_description, notes, price, category_id, negotiable, time_frame_date, time_frame_date, location, date_posted)
-	#	STATUS:			NEEDS TESTING
+	#	SUMMARY:		Adds tokens to a user's account
+	#	INPUTS:			INT num_tokens, user_id
+	#	OUTPUTS:		
+	#	STATUS:			IN PROGRESS
     ##########
-	function addTokens()
+	function addTokens($user_id)
 	{
 	$request = \Slim\Slim::getInstance()->request();
-	$sql = "SELECT * FROM TASK GROUP BY task_id ORDER BY MAX(date_posted) LIMIT :num_tasks";
+	$sql = "SELECT tokens FROM USER WHERE user_id = :user_id;";
 	try
 		{
-			$num_tasks = intval($num_tasks);
 			$db = getConnection();
 			$stmt= $db->prepare($sql);
-			$stmt->bindParam('num_tasks', $num_tasks, PDO::PARAM_INT);
+			$stmt->bindParam('user_id', $user_id);
 			$stmt->execute();
-			$recentTasks = null;
-			while($row = $stmt->fetch(PDO::FETCH_ASSOC)) //I'm not 100% sure about this line but I'm using login as a guide for this
-			{
-				$taskID = $row['task_id'];
-				$recentTasks[$taskID] = array('beggar_id' => (int)$row['beggar_id'], 
-											  'chooser_id' => (int)$row['chooser_id'], 
-											  'short_description' => $row['short_description'], 
-											  'notes' => $row['notes'], 
-											  'price' => (int)$row['price'], 
-											  'category_id' => (int)$row['category_id'], 
-											  'negotiable' => (int)$row['negotiable'], 
-											  'time_frame_date' => $row['time_frame_date'], 
-											  'time_frame_time' => $row['time_frame_time'], 
-											  'location' => $row['location'],
-											  'date_posted' => $row['date_posted']);
-			}
+			$user_info = $stmt->fetch(PDO::FETCH_ASSOC);
 			$db = null;
-		   	echo json_encode($recentTasks);
+			$user_tokens = array('tokens' => $userinfo['tokens']);
+		   	echo json_encode($user_tokens);
 	      	
       	}
 	catch(PDOException $e) 
