efdd689469189a173d5d588e10cdc36683007e80
Chris Dunn <christopherdunn711@gmail.com>
2014-04-18 14:14:09 -0500

    update on tokens

---
+++ tokens.php
@@ -0,0 +1,16 @@
+	<?php include "components/header.html" ?>
+	<script language="javascript"  type="text/javascript" src="js/settings.js"> </script> 
+</head>
+<body class="blue">
+	<!--<span class="currentPage" id="accountPage"></span>-->
+
+<div class="row yellow">
+	<div class="twelve columns">
+		<p>CHANGE IS GOOD!</p>	
+	</div>
+</div>
+<br>
+
+</body>
+</html>
+
