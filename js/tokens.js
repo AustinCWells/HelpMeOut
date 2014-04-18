41bb152e2abac032c9729a1648caafe3fa6e0826
Chris Dunn <christopherdunn711@gmail.com>
2014-04-18 17:51:38 -0500

    tokens stuff

--- js/tokens.js
+++ js/tokens.js
@@ -3,6 +3,69 @@ $(document).ready(function(){
 	$("#paypay").submit(function(event){
 		event.preventDefault();
 		alert("This option is not live please use the on the left to update tokens");
+		var purchase = parseInt($("#tokenAmount").val(), 10);
+		console.log(purchase);
+
+		$.ajax({
+		type: 'POST',
+		url: url,
+		content: content,
+		data: JSON.stringify(json),
+		success: function(data){
+
+			console.log(data);
+
+			var obj = JSON.parse(data);
+
+
+
+			if(Object.keys(obj)[0] === "error"){
+				obj.modal = "Error occured while adding your tokens";
+
+				displayError(obj);
+			}
+
+			else{
+
+				if(type === "jobPost"){
+					obj.modal = "Job Post was SuccesFul!";
+					displaySuccess(obj);
+				}
+
+				else if(type === "login") {
+
+					if(obj.userID){
+						$.cookie("userInfo", obj);
+						login();
+					}
+
+					else{
+						obj.modal = "Login in Failure!";
+						displayError(obj);
+					}
+
+				}
+
+				else if(type === "signUp"){
+					$.cookie("userInfo", obj);
+					login();
+					obj.modal = "Sign Up was SuccesFul!";
+					displaySuccess(obj);
+				}
+
+			}
+
+		},
+
+		//CAN WE MAKE THIS ERROR MESSAGE MORE SPECIFIC?
+		//IF THIS IS BECAUSE OF A BROKEN LINK BETWEEN
+			//MODALS AND API, CAN WE MENTION THAT?
+
+		error: function(data){
+			alert("WE'RE SORRY SOMETHING WENT WRONG!");
+		}
+
+	});
 	});
 
 });
--- tokens.php
+++ tokens.php
@@ -1,5 +1,6 @@
 	<?php include "components/header.html" ?>
 	<script language="javascript"  type="text/javascript" src="js/tokens.js"> </script> 
+		<link href="css/tokens.css" rel="stylesheet">
 </head>
 <body class="blue">
 	<!--<span class="currentPage" id="accountPage"></span>-->
@@ -10,23 +11,58 @@
 	</div>
 </div>
 <br>
+<div class="row">
 
-<form action="https://www.paypal.com/cgi-bin/webscr" method="post" id = "paypay" target="_top">
-<input type="hidden" name="cmd" value="_s-xclick">
-<table>
-<tr><td><input type="hidden" name="on0" value="Token Prices">Token Prices</td></tr><tr><td><select name="os0">
-	<option value="10 Tokens">10 Tokens $5.00 USD</option>
-	<option value="25 Tokens">25 Tokens $10.00 USD</option>
-	<option value="50 Tokens">50 Tokens $15.00 USD</option>
-	<option value="100 Tokens">100 Tokens $20.00 USD</option>
-</select> </td></tr>
-</table>
-<input type="hidden" name="currency_code" value="USD">
-<input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIIGQYJKoZIhvcNAQcEoIIICjCCCAYCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYBVyC+/PONbE/Pg+quZOhFLA1uxqUD47Lbk2hGkt2HLMphrx2b/KUoE8PNzDhz7QAISaKQ1H4olxm2kra6927r7Wj1L8bGqpetPEKvZ2J3W5Wp3JdHcdPOa4MMFaZKHDIC+od//Zd9TBMp1gE+4kj1+KSJhiiRbwxYPwTB47hIl7jELMAkGBSsOAwIaBQAwggGVBgkqhkiG9w0BBwEwFAYIKoZIhvcNAwcECAD2DzaVfaDFgIIBcJhFDYCjxUWXYJZ1K9Uo0w8jUFE6lGcfrHmbiIHVcKH1/Bcjb+vShOgdUEb2Twcq76uQWiRaW7S81FuZZnbaHCcXS0j449U4KLFeMaLzJGNtV8DRde8hTeXXHZvSKNqqb4ZgUbsoctR9zJ6o1JuZDeGX3Tr+Mu+uUVpMWEMVzin0kdjsczpmeid/z2dK0oO/GGuD2EnJ+e7IJKKgHFLXRha6pf93aCUiAtNyuW3VPkm0GfsFAXazjfuM62rw+FZ3v4zMQpfnXfggQhw58b4hTrkZ7srXqeMIR6cc8J+yISUgQKdA1Oz8Xv5dLdEeEYyTF0dO57SHqH4hGlVF63n1KRILg3pzUrTGrxncrx1Jf+LBJmHRORSD5GrJkmCtTbx324bUmqxQ+5IlE4jEuXaPy6ZI57yUBJkZqcC1wi48jD2uI0TZhSBwEzXxIkghaY+/D1s4fbi33L/v7Q+qsM5PK7VdF2PwcO4FC5tnAKv9lqwqoIIDhzCCA4MwggLsoAMCAQICAQAwDQYJKoZIhvcNAQEFBQAwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMB4XDTA0MDIxMzEwMTMxNVoXDTM1MDIxMzEwMTMxNVowgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBR07d/ETMS1ycjtkpkvjXZe9k+6CieLuLsPumsJ7QC1odNz3sJiCbs2wC0nLE0uLGaEtXynIgRqIddYCHx88pb5HTXv4SZeuv0Rqq4+axW9PLAAATU8w04qqjaSXgbGLP3NmohqM6bV9kZZwZLR/klDaQGo1u9uDb9lr4Yn+rBQIDAQABo4HuMIHrMB0GA1UdDgQWBBSWn3y7xm8XvVk/UtcKG+wQ1mSUazCBuwYDVR0jBIGzMIGwgBSWn3y7xm8XvVk/UtcKG+wQ1mSUa6GBlKSBkTCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb22CAQAwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOBgQCBXzpWmoBa5e9fo6ujionW1hUhPkOBakTr3YCDjbYfvJEiv/2P+IobhOGJr85+XHhN0v4gUkEDI8r2/rNk1m0GA8HKddvTjyGw/XqXa+LSTlDYkqI8OwR8GEYj4efEtcRpRYBxV8KxAW93YDWzFGvruKnnLbDAF6VR5w/cCMn5hzGCAZowggGWAgEBMIGUMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMTQwNDE4MjExNzU2WjAjBgkqhkiG9w0BCQQxFgQUI2N1QU3M/AvIz+IJOueDmEcyRicwDQYJKoZIhvcNAQEBBQAEgYCXarqJZiRoarmSMXURt39QP6U8/8BLwMJdwQ3k74H0JMuMDCcg/iGNkVVutzN69QPWVjVN9mQGrDh+x4Lsw61bf8oKF5zVedyR1EIkHWWq4kBaRLeMDH24fM8cyP1HeYK1kngpRjpRE9O0ye7YG9YvR/wsT58x6x2z4zkyKgHWYQ==-----END PKCS7-----
-">
-<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-</form>
+	<div class="six columns">
+
+
+		<div id ="buyDiv">
+
+			<div id="tokenPrices">
+
+				<ol>Prices 
+					<li>10 Tokens for $5.00</li>
+					<li>25 Tokens for $10.00</li>
+					<li>50 Tokens for $15.00</li>
+					<li>100 Tokens for $20.00</li>
+				</ol>
+
+			</div>
+
+			<div id="tokenBuy">
+				<form action="https://www.paypal.com/cgi-bin/webscr" method="post" id = "paypay" target="_top">
+					<input type="hidden" name="cmd" value="_s-xclick">
+					<table>
+						<tr>
+							<td>
+								<input type="hidden" name="on0" value="Token Prices">Token Prices
+							</td>
+						</tr>
+
+						<tr>
+							<td>
+								<select id="tokenAmount" name="os0">
+					<option value="10">10 Tokens $5.00 USD</option>
+					<option value="25">25 Tokens $10.00 USD</option>
+					<option value="50">50 Tokens $15.00 USD</option>
+					<option value="100">100 Tokens $20.00 USD</option></select>
+							</td>
+						</tr>
+					</table>
+
+					<!--<input type="hidden" name="currency_code" value="USD">
+					<input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIIGQYJKoZIhvcNAQcEoIIICjCCCAYCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYBVyC+/PONbE/Pg+quZOhFLA1uxqUD47Lbk2hGkt2HLMphrx2b/KUoE8PNzDhz7QAISaKQ1H4olxm2kra6927r7Wj1L8bGqpetPEKvZ2J3W5Wp3JdHcdPOa4MMFaZKHDIC+od//Zd9TBMp1gE+4kj1+KSJhiiRbwxYPwTB47hIl7jELMAkGBSsOAwIaBQAwggGVBgkqhkiG9w0BBwEwFAYIKoZIhvcNAwcECAD2DzaVfaDFgIIBcJhFDYCjxUWXYJZ1K9Uo0w8jUFE6lGcfrHmbiIHVcKH1/Bcjb+vShOgdUEb2Twcq76uQWiRaW7S81FuZZnbaHCcXS0j449U4KLFeMaLzJGNtV8DRde8hTeXXHZvSKNqqb4ZgUbsoctR9zJ6o1JuZDeGX3Tr+Mu+uUVpMWEMVzin0kdjsczpmeid/z2dK0oO/GGuD2EnJ+e7IJKKgHFLXRha6pf93aCUiAtNyuW3VPkm0GfsFAXazjfuM62rw+FZ3v4zMQpfnXfggQhw58b4hTrkZ7srXqeMIR6cc8J+yISUgQKdA1Oz8Xv5dLdEeEYyTF0dO57SHqH4hGlVF63n1KRILg3pzUrTGrxncrx1Jf+LBJmHRORSD5GrJkmCtTbx324bUmqxQ+5IlE4jEuXaPy6ZI57yUBJkZqcC1wi48jD2uI0TZhSBwEzXxIkghaY+/D1s4fbi33L/v7Q+qsM5PK7VdF2PwcO4FC5tnAKv9lqwqoIIDhzCCA4MwggLsoAMCAQICAQAwDQYJKoZIhvcNAQEFBQAwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMB4XDTA0MDIxMzEwMTMxNVoXDTM1MDIxMzEwMTMxNVowgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBR07d/ETMS1ycjtkpkvjXZe9k+6CieLuLsPumsJ7QC1odNz3sJiCbs2wC0nLE0uLGaEtXynIgRqIddYCHx88pb5HTXv4SZeuv0Rqq4+axW9PLAAATU8w04qqjaSXgbGLP3NmohqM6bV9kZZwZLR/klDaQGo1u9uDb9lr4Yn+rBQIDAQABo4HuMIHrMB0GA1UdDgQWBBSWn3y7xm8XvVk/UtcKG+wQ1mSUazCBuwYDVR0jBIGzMIGwgBSWn3y7xm8XvVk/UtcKG+wQ1mSUa6GBlKSBkTCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb22CAQAwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOBgQCBXzpWmoBa5e9fo6ujionW1hUhPkOBakTr3YCDjbYfvJEiv/2P+IobhOGJr85+XHhN0v4gUkEDI8r2/rNk1m0GA8HKddvTjyGw/XqXa+LSTlDYkqI8OwR8GEYj4efEtcRpRYBxV8KxAW93YDWzFGvruKnnLbDAF6VR5w/cCMn5hzGCAZowggGWAgEBMIGUMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMTQwNDE4MjExNzU2WjAjBgkqhkiG9w0BCQQxFgQUI2N1QU3M/AvIz+IJOueDmEcyRicwDQYJKoZIhvcNAQEBBQAEgYCXarqJZiRoarmSMXURt39QP6U8/8BLwMJdwQ3k74H0JMuMDCcg/iGNkVVutzN69QPWVjVN9mQGrDh+x4Lsw61bf8oKF5zVedyR1EIkHWWq4kBaRLeMDH24fM8cyP1HeYK1kngpRjpRE9O0ye7YG9YvR/wsT58x6x2z4zkyKgHWYQ==-----END PKCS7-----
+					">-->
+					<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+					<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+				</form>
+			</div>
+
+			<div class = "clear"></div>
+
+		</div>
+	</div>
+</div>
 
 
 </body>
