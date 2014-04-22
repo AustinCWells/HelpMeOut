faff0265117dd716880933abd55bf3a703a08ddb
SpencerKaiser <skaiser@me.com>
2014-04-22 18:22:40 -0500

    Changed is_custom to an int

--- api/index.php
+++ api/index.php
@@ -236,7 +236,7 @@
 							  'gender' => $userInfo['gender'], 
 							  'times_reported' => $userInfo['times_reported'], 
 							  'tokens' => (int)$userInfo['tokens'],
-							  'is_custom' => $userInfo['is_custom'],
+							  'is_custom' => (int)$userInfo['is_custom'],
 							  'custom_image_path' => $userInfo['custom_image_path']);
 			echo json_encode($response);
 		}
