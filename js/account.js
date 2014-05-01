14ee83d56d4a97647e11b89ff196c1fcfa21e57d
Jordan Silver <Jordan@Jordans-MacBook-Pro.local>
2014-04-30 22:35:19 -0500

    Pushing account.js changes

--- js/account.js
+++ js/account.js
@@ -8,48 +8,61 @@ $(window).ready(function(event) {
 	$("#navAccount").addClass("currentPage")
 	$("#navAccount").removeClass("hoverable");
 
-	console.log("is_custom = " + userInfo.is_custom);
-	if(userInfo.is_custom === 0) {
-		$("#profilePic").attr("src", "img/fontenot.jpeg");
-	}
-	else {
-		$("#profilePic").attr("src", userInfo.custom_image_path);//TODO: load custom profile image
-	}
-	var contactInfoHTML =  "Name: " + userInfo.firstName + ' ' + userInfo.lastName + "<br>Phone #: <span id='phone'>" + userInfo.phone + "</span><br>Email: " + userInfo.email;
-	$("#contactInfo").append(contactInfoHTML); 
+	var userJSON = {};
+	userJSON.user_id = userInfo.userID;
+	console.log(JSON.stringify(userJSON));
 
-	$('#phone').text(function(i, text) {
-	    return text.replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3');
+	$.ajax({
+		type: 'POST',
+		url: "api/useraccount",
+		content: 'application/json',
+		data: JSON.stringify(userJSON),
+		success: function(data){
+			console.log(data);
+
+			console.log("is_custom = " + userInfo.is_custom);
+			if(userInfo.is_custom === 0) {
+				$("#profilePic").attr("src", "img/fontenot.jpeg");
+			}
+			else {
+				$("#profilePic").attr("src", userInfo.custom_image_path);//TODO: load custom profile image
+			}
+			var contactInfoHTML =  "Name: " + userInfo.firstName + ' ' + userInfo.lastName + "<br>Phone #: <span id='phone'>" + userInfo.phone + "</span><br>Email: " + userInfo.email;
+			$("#contactInfo").append(contactInfoHTML); 
+
+			$('#phone').text(function(i, text) {
+			    return text.replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3');
+			});
+
+			var badges = [1,2,3];
+			var badgeSpace = $('#badgeSpace');
+
+			for(var i=0;i<badges.length;i++) {
+				badgeSpace.css("margin-left",(parseFloat(badgeSpace.css("margin-left")) - 100) + 'px');
+				if(i===0) {
+					badgeSpace.empty();
+				}
+				if (badges[i] === 1) {
+					var badgeSrc = "img/multitasker.png";
+					var badgeName = "Multitasker";
+					var badgeDesc = "You take on multiple requests at a time";
+				}
+				else if (badges[i] === 2) {
+					var badgeSrc = "img/nightowl.png";
+					var badgeName = "Night Owl";
+					var badgeDesc = "You Work the Graveyard Shift";
+				}
+				else if (badges[i] === 3) {
+					var badgeSrc = "img/speeddemon.png";
+					var badgeName = "Speed Demon";
+					var badgeDesc = "They call you... 'El Rapido'";
+				}
+
+				var badgeHTML = '<div class="badge" id="badge' + badges[i] + '"><img class="badgeImg" src="' + badgeSrc + '"><br><div class="badgeName">' + badgeName + ':</div><br><div class="badgeDesc">' + badgeDesc + '</div></div>';
+				badgeSpace.append(badgeHTML);
+
+			}
+		}
 	});
-
-	var badges = [1,2,3];
-	var badgeSpace = $('#badgeSpace');
-
-	for(var i=0;i<badges.length;i++) {
-		badgeSpace.css("margin-left",(parseFloat(badgeSpace.css("margin-left")) - 100) + 'px');
-		if(i===0) {
-			badgeSpace.empty();
-		}
-		if (badges[i] === 1) {
-			var badgeSrc = "img/multitasker.png";
-			var badgeName = "Multitasker";
-			var badgeDesc = "You take on multiple requests at a time";
-		}
-		else if (badges[i] === 2) {
-			var badgeSrc = "img/nightowl.png";
-			var badgeName = "Night Owl";
-			var badgeDesc = "You Work the Graveyard Shift";
-		}
-		else if (badges[i] === 3) {
-			var badgeSrc = "img/speeddemon.png";
-			var badgeName = "Speed Demon";
-			var badgeDesc = "They call you... 'El Rapido'";
-		}
-
-		var badgeHTML = '<div class="badge" id="badge' + badges[i] + '"><img class="badgeImg" src="' + badgeSrc + '"><br><div class="badgeName">' + badgeName + ':</div><br><div class="badgeDesc">' + badgeDesc + '</div></div>';
-		badgeSpace.append(badgeHTML);
-
-	}
-
 
 });
